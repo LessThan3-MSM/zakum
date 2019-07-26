15663eedba091c4c3c16796fb8059a332fcc46e5
tommymurphy <temurph1@asu.edu>
2019-07-25 22:23:03 -0500

    Balancing funcionality and creation of groups

--- bot.js
+++ bot.js
@@ -6,45 +6,23 @@ var fs = require("fs");
 
 let groups = [];
 let pool = [];
+let waitlist = [];
 
 client.once('ready', () => {
 	console.log('Ready!');
 	const leaders = lt3.filter(member => member.leader)
 	pool.push(leaders[0])
 	pool.push(leaders[1])
-	console.log(pool)
 });
 
 client.on('message', message => {
 	if (message.author.bot) return;
   if (message.content.substring(0,5) === `${prefix}join` && message.content.substring(0,7) !== `${prefix}joined` ) {
-		const roster = getRoster()
-		let user = null;
-		let name = null;
-
-		if (message.content.split(" ").length > 1){
-			const added = roster.find(member => member.name.toLowerCase() === message.content.split(" ")[1].toLowerCase())
-			name = message.content.split(" ")[1]
-			user = added ? added.id:null
-		} else {
-			user = message.author.username + "#" + message.author.discriminator
-		}
-
-		const joined = roster.find(member => member.id === user)
-		if (!joined){
-			message.channel.send(`${name || message.author.username} does not appear to be on the guild roster. Please contact your guild leader to get added to the roster.`)
-			return;
-		}
-
-		if (pool.find( member => member.id === joined.id  )){
-			pool = pool.filter(member => member.id !== joined.id)
-			message.channel.send(`Removed ${name || message.author.username} from the Zakum Expedition Finder queue.`)
-			return;
-		} else {
-			pool.push(joined)
-			balance(pool)
-			message.channel.send(`${name || message.author.username} has joined the Zakum Expedition Finder queue! :heart:`)
-		}
+			const roster = getRoster()
+			message.content.split(" ").forEach(function (joiner, index){
+			message.content.split(" ").length === 1 && addMemberToPool(null, message, roster)
+			message.content.split(" ").length > 1 && joiner.length > 1 && index !== 0 && addMemberToPool(joiner, message, roster)
+		})
 	}
 
 	if (message.content.substring(0,4) === `${prefix}add`){
@@ -104,15 +82,29 @@ client.on('message', message => {
 	if (message.content === `${prefix}groups`) {
 		const group1 = formatGroupMessage("Group 1", groups[0] || pool)
 		const group2 = formatGroupMessage("Group 2", groups[1])
-		const msg = `Group 1: ${group1}`
-		if (groups[1]){
-			msg += "\n \nGroup 2: \n" + group2
+		const waitlistGroup = formatGroupMessage("Waitlist", waitlist)
+		const differenceMsg = formatDifferenceMessage(computeDifference(groups[0], groups[1], true))
+		let msg = `Group 1: ${group1} (${group1.length})`
+		if (groups[1] && groups[1].length){
+			msg += `\nGroup 2: ${group2} (${group2.length})`
 		}
+		if (waitlist && waitlist.length){
+			msg += `\nWaitlist: ${waitlistGroup} (${waitlistGroup.length})`
+		}
+		console.log("```" + msg + "```")
 		message.channel.send("```" + msg + "```")
+		groups[1] && message.channel.send(`\`${differenceMsg}\``)
 	}
 
 	if (message.content.substring(0,5) === `${prefix}pool` || message.content.substring(0,7) === `${prefix}joined`) {
 		pool.length && message.channel.send(pool.map(joined => joined.name).sort())
+	}
+
+	if (message.content.substring(0,6) === `${prefix}reset`) {
+		const leaders = lt3.filter(member => member.leader)
+		pool = [leaders[0], leaders[1]]
+		groups = [];
+		waitlist = [];
 	}
 
 	if (message.content.substring(0,5) === `${prefix}find`) {
@@ -125,19 +117,119 @@ client.on('message', message => {
 	}
 
 	function formatGroupMessage(name, group) {
-		return group ? group.map(member => member.name).sort():[]
+		return group ? group.map(member => member.name).sort((a,b) => b.rank - a.rank):[]
 	}
 
-	function balance(pool){
+	function formatDifferenceMessage(difference){
+		if(difference === 0){
+			return "Zakum has assembled absolutely perfect and balanced groups!"
+		}
+		else if (difference > 0 && difference < 2){
+			return "Zakum has put together wonderful groups for the expedition!"
+		}
+		else if (difference > 2 && difference < 4){
+			return "Zakum created balanced groups for the expedition!"
+		}
+		else {
+			return "Zakum tried his best to balance groups :("
+		}
+	}
+
+	function balance(pool, message){
+		groups[0] = [];
+		groups[1] = [];
 		if (pool.length <= 10){
 			groups[0] = pool
+		} else {
+			message.channel.send("Zakum has detected two groups! Balancing groups...")
+			let difference = 1000;
+			const leaders = lt3.filter(member => member.leader)
+			let g0 = [leaders[0]]
+			let g1 = [leaders[1]]
+			let poolToJoin = pool.filter(person => person !== leaders[0] && person !== leaders[1]).sort((a,b) => b.rank-a.rank)
+
+			while(poolToJoin.length){
+				let diff = computeDifference(g0, g1, false)
+				var max = poolToJoin.sort((a,b) => b.rank-a.rank)[0].rank
+				if(g0.length === 1 && g1.length === 1){
+					const memberToJoin = poolToJoin.find(member => member.rank === max)
+					g0.push(memberToJoin)
+					poolToJoin = poolToJoin.filter(member => member !== memberToJoin)
+				}
+				else if (g0.length === 10){
+					const memberToJoin = poolToJoin.find(member => member.rank === max)
+					g1.push(memberToJoin)
+					poolToJoin = poolToJoin.filter(member => member !== memberToJoin)
+				}
+				else if (g1.length === 10){
+					const memberToJoin = poolToJoin.find(member => member.rank === max)
+					g0.push(memberToJoin)
+					poolToJoin = poolToJoin.filter(member => member !== memberToJoin)
+				}
+				else if (diff > 0) {
+					const memberToJoin = poolToJoin.find(member => member.rank === max)
+					g1.push(memberToJoin)
+					poolToJoin = poolToJoin.filter(member => member !== memberToJoin)
+				}
+				else {
+					const memberToJoin = poolToJoin.find(member => member.rank === max)
+					g0.push(memberToJoin)
+					poolToJoin = poolToJoin.filter(member => member !== memberToJoin)
+				}
+			}
+			groups[0] = g0
+			groups[1] = g1
 		}
+	}
+
+	function computeDifference(group1, group2, abs){
+		const g1 = group1 && group1.reduce(function (acc, obj) { return acc + obj.rank; }, 0);
+		const g2 = group2 && group2.reduce(function (acc, obj) { return acc + obj.rank; }, 0);
+		return abs ? Math.abs(g1-g2) : (g1-g2)
 	}
 
 	function getRoster(){
 		return JSON.parse(fs.readFileSync('./guilds/lt3.json', 'utf8')).lt3
 	}
 
+	function addMemberToPool(name, message, roster){
+		let user = null;
+		if (name){
+			const added = roster.find(member => member.name.toLowerCase() === name.toLowerCase())
+			user = added ? added.id:null
+		} else {
+			user = message.author.username + "#" + message.author.discriminator
+		}
+
+		const joined = roster.find(member => member.id === user)
+		if (!joined){
+			message.channel.send(`${name || message.author.username} does not appear to be on the guild roster. Please contact your guild leader to get added to the roster.`)
+			return;
+		}
+
+		if(waitlist && waitlist.find( member => member.id === joined.id  )){
+			message.channel.send(`Removed ${name || message.author.username} from the Zakum Expedition Finder waitlist.`)
+			waitlist = waitlist.filter(member => member.id !== joined.id)
+		}
+		else if (pool.find( member => member.id === joined.id  )){
+			pool = pool.filter(member => member.id !== joined.id)
+			message.channel.send(`Removed ${name || message.author.username} from the Zakum Expedition Finder queue.`)
+			if(waitlist && waitlist.length){
+				pool.push(waitlist[0])
+				waitlist = waitlist.filter(member => member !== waitlist[0])
+			}
+			balance(pool, message)
+			return;
+		} else if (groups[0] && groups[1] && groups[0].length === 10 && groups[1].length === 10){
+			message.channel.send(`Sorry ${name || message.author.username}! Looks like we've reached capacity. Adding you to the waitlist!`)
+			waitlist.push(joined)
+			return;
+		} else {
+			pool.push(joined)
+			balance(pool, message)
+			message.channel.send(`${name || message.author.username} has joined the Zakum Expedition Finder queue! :heart:`)
+		}
+	}
 });
 
 client.login(token);
