70e84b6dc5c999017c21a1fd0d85b528948faabc
Bryan Leung <bryanleung92@gmail.com>
2019-08-23 22:56:45 -0700

    Adding a leaderboard command

---
+++ commands/leaderboard.js
@@ -0,0 +1,35 @@
+var fs = require("fs");
+
+module.exports = {
+  leaderboard: function (message, roster) {
+
+    if(message.content.split(" ").length !== 2) { // we want users to input how many
+      message.channel.send("Please use like so: !leaderboard <positive integer>");
+      return;
+    }
+
+    let stringy = "List of beautiful whales <3\n```";
+    let amount = message.content.split(" ")[1];
+    amount = Math.min(amount, roster.length); //prevents out of index errors
+
+    if (!parseInt(amount)) { //for NaN inputs
+      message.channel.send("Use a positive WHOLE number you twat");
+      return;
+    }
+
+    if (parseInt(amount) < 1) { // for 0 or negatives
+      message.channel.send("Stop it. Use a positive WHOLE number");
+      return;
+    }
+
+    roster = roster.sort((a, b) => b.rank - a.rank);
+    for (i = 0; i < amount; i++) {
+      const place = i + 1
+      stringy += "#" + place + " " + roster[i].name + " ".repeat(Math.abs(roster[i].name.length - 13)) + roster[i].rank + "\n";
+    }
+
+    stringy += "```";
+    message.channel.send(stringy);
+		return;
+  }
+};
