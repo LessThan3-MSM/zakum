910aeb14788c052fcaa89ddc8fbf2fc8b45518d2
jacqio <elizabeth.m.vincent@gmail.com>
2019-12-22 23:12:05 -0500

    2000 char limit
    fixed 2000 char limit. some formatting. added optional parameter display.

--- commands/commands.js
+++ commands/commands.js
@@ -4,7 +4,22 @@ module.exports = {
   listCommands: function (channel, isUserAdmin) {
     let helpMsg = '__Zakum Commands__ \n';
     const commandsCanAccess = !isUserAdmin ? commands.filter(command => command.admin === false) : commands
-    commandsCanAccess.forEach(command => helpMsg += `**!${command.name}** ${command.required && `**{${command.required.join(", ")}}**`}  : ${command.description} \n` )
-    channel.send(helpMsg);
+
+    commandsCanAccess.forEach( function(command){
+      var newMsg = `**!${command.name}**${command.required && command.required.length>0 ? ` **{${command.required.join(", ")}}**` : ''}${command.optional && command.optional.length>0 ? ` *****{${command.optional.join(", ")}}*****`:''}: ${command.description} \n`
+       if((helpMsg + newMsg).length > 2000){
+         channel.send(helpMsg);
+         helpMsg = newMsg;
+       }else{
+         helpMsg += newMsg;
+       }
+     });
+     channel.send(helpMsg);
+
+    //helpMsg = helpMsg.match(/[\s\S]{1,2000}/g) || [];
+  //  for(var i = 0; i < helpMsg.length; i++){
+    //    channel.send(helpMsg[i]);
+  //  }
+
   }
 };
