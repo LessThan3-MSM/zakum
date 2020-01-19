f5e1ed13d0bdf830865cada346c75d8508e70bff
jacqio <elizabeth.m.vincent@gmail.com>
2020-01-19 16:03:03 -0500

    move - waitlist
    added waitlist targeting.

--- commands/move.js
+++ commands/move.js
@@ -1,6 +1,7 @@
 module.exports = {
   move: function (channel, guildData, memberName, targetMemberName, guildID){
     var groups = guildData.groups;
+    const waitlist = 'waitlist';
 
     channel.send("Zakum used **Move!**")
 
@@ -10,7 +11,7 @@ module.exports = {
     }
 
     var sourceGroupID = -1;
-    var targetGroupID = -1;
+    var targetGroupID = targetMemberName.toLowerCase() === waitlist ? waitlist : -1;
     var isOnlyLeader = false;
     var sourceMember;
 
@@ -24,11 +25,11 @@ module.exports = {
     if(sourceGroupID == -1 || targetGroupID == -1){ //try the waitlist?
       const p1 = guildData.waitlist.find(member => member.name.toLowerCase() === memberName.toLowerCase())
       const p2 = guildData.waitlist.find(member => member.name.toLowerCase() === targetMemberName.toLowerCase())
-      if(p1) { sourceGroupID = 'wl'; sourceMember = p1; isOnlyLeader = p1.leader && guildData.waitlist.filter(member => member.leader).length == 1; }
-      if(p2) targetGroupID = 'wl';
+      if(p1) { sourceGroupID = waitlist; sourceMember = p1; isOnlyLeader = p1.leader && guildData.waitlist.filter(member => member.leader).length == 1; }
+      if(p2) targetGroupID = waitlist;
     }
 
-    if (sourceGroupID === targetGroupID || targetGroupID !== 'wl' && groups[targetGroupID].length >= 10) {
+    if (sourceGroupID === targetGroupID || targetGroupID !== waitlist && groups[targetGroupID].length >= 10) {
       channel.send("*The attack missed!*")
       return;
     }else if (isOnlyLeader){
@@ -37,14 +38,14 @@ module.exports = {
     }
 
     //add target to new group.
-    if(targetGroupID === 'wl'){
+    if(targetGroupID === waitlist){
       guildData.waitlist.push(sourceMember);
     }else{
       groups[targetGroupID].push(sourceMember);
     }
 
     //remove from original group.
-    if(sourceGroupID === 'wl'){
+    if(sourceGroupID === waitlist){
       guildData.waitlist = guildData.waitlist.filter(member => member.name.toLowerCase() !== memberName.toLowerCase());
     }else{
       groups[sourceGroupID] = groups[sourceGroupID].filter(member => member.name.toLowerCase() !== memberName.toLowerCase())
