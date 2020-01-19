ab6f299f926cb6a016275a7e78778bd318b4248e
jacqio <elizabeth.m.vincent@gmail.com>
2020-01-19 16:28:52 -0500

    group move by number
    allow group move by number

--- commands/move.js
+++ commands/move.js
@@ -11,25 +11,27 @@ module.exports = {
     }
 
     var sourceGroupID = -1;
-    var targetGroupID = targetMemberName.toLowerCase() === waitlist ? waitlist : -1;
+    var targetGroupID = targetMemberName.toLowerCase() === waitlist ? waitlist : parseInt(targetMemberName) ? parseInt(targetMemberName)-1 : -1;
     var isOnlyLeader = false;
     var sourceMember;
 
     groups.forEach((group, index) => {
       const p1 = group.find(member => member.name.toLowerCase() === memberName.toLowerCase())
-      const p2 = group.find(member => member.name.toLowerCase() === targetMemberName.toLowerCase())
+      var p2;
+      if(targetGroupID == -1) p2 = group.find(member => member.name.toLowerCase() === targetMemberName.toLowerCase())
       if(p1) { sourceGroupID = index; sourceMember = p1; isOnlyLeader = p1.leader && group.filter(member => member.leader).length == 1; }
       if(p2) targetGroupID = index;
     });
 
     if(sourceGroupID == -1 || targetGroupID == -1){ //try the waitlist?
       const p1 = guildData.waitlist.find(member => member.name.toLowerCase() === memberName.toLowerCase())
-      const p2 = guildData.waitlist.find(member => member.name.toLowerCase() === targetMemberName.toLowerCase())
+      var p2;
+      if(targetGroupID == -1) p2 = guildData.waitlist.find(member => member.name.toLowerCase() === targetMemberName.toLowerCase())
       if(p1) { sourceGroupID = waitlist; sourceMember = p1; isOnlyLeader = p1.leader && guildData.waitlist.filter(member => member.leader).length == 1; }
       if(p2) targetGroupID = waitlist;
     }
 
-    if (sourceGroupID === targetGroupID || targetGroupID !== waitlist && groups[targetGroupID].length >= 10) {
+    if (sourceGroupID === targetGroupID || sourceGroupID < 0 || targetGroupID < 0 || (targetGroupID !== waitlist && (groups[targetGroupID]==null || groups[targetGroupID].length >= 10))) {
       channel.send("*The attack missed!*")
       return;
     }else if (isOnlyLeader){
